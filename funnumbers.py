59d62d936063a36f826c68517e67906c2c6a4c2e
William <willi_vs1997@hotmail.com>
2017-02-27 10:52:46 -0600

    Add files via upload
    ~~FUN WITH NUMBERS~~

---
+++ funnumbers.py
@@ -0,0 +1,29 @@
+# This program calculates the sum of 2 numbers
+print("THE SUM OF NUMBERS")
+x = int(input("Dame el primer numero: "))
+y = int(input("Dame el segundo numero: "))
+print("el resultado es: ", x+y)
+
+# This program calculates the difference of 2 numbers
+print("THE DIFFERENCE OF NUMBERS")
+x = int(input("Dame el primer numero: "))
+y = int(input("Dame el segundo numero: "))
+print("El resultado es: ", x-y)
+
+# This program calculates the product of 2 numbers
+print("THE PRODUCT OF NUMBERS")
+x = int(input("Dame el primer numero: "))
+y = int(input("Dame el segundo numero: "))
+print("El resultado es: ", x*y)
+
+# This program calculates the division between 2 numbers
+print("THE DIVISION OF NUMBERS")
+x = int(input("Dame el primer numero: "))
+y = int(input("Dame el segundo numero: "))
+print("El resultado es: ", x/y)
+
+# This program calculates the remainder of the division between 2 numbers
+print("THE REMAINDER OF NUMBERS")
+x = int(input("Dame el primer numero: "))
+y = int(input("Dame el segundo numero: "))
+print("El resultado es: ", x%y)
